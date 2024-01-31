ca2bb01376abb0a9aae0fb7448c092b6ef143cd2
Tyler Savitsky <tsavitsky11@gmail.com>
2024-01-30 22:27:28 -0500

    fixed webpack issues, added a boilerplate server, and fixed proxy server

--- src/MainPageContainer.tsx
+++ src/MainPageContainer.tsx
@@ -8,14 +8,15 @@ export default function MainContainer():React.JSX.Element {
 
   // get state
   const [data, setData] = useState([['API1API1API1API1API1API1API1API1API1API1API1API1API1API1API1API1API1', 
-  'Status1', 'Location1'], ['API2', 'Status2', 'Location2'], ['API3', 'Status3', 'Location3']])
+  'Status1', 'Location1'], ['API2', 'Status2', 'Location2'], ['API3', 'Status3', 'Location3']]);
+  const [backgroundColor, setBackgroundColor] = useState(['white', 'lightblue']);
   //make fetch request to get our data somewhere in here too
 
   //iterate through state to make all of our rows, and push them into the array
   for(const subarray of data){
     // for each subarray, create a new row, passing in the data from data, 
     // which we get from a fetch request to the back end
-    rows.push(<Column api={subarray[0]} status={subarray[1]} location={subarray[2]}/>)
+    rows.push(<Column api={subarray[0]} status={subarray[1]} location={subarray[2]} backgroundColor={backgroundColor[0]}/>)
   }
 
   // create the header row
--- src/Row.tsx
+++ src/Row.tsx
@@ -4,13 +4,14 @@ type ColumnProps = {
   api: string,
   location: string,
   status: string,
+  backgroundColor: string
 }
 
 export default function Column(props:ColumnProps):React.JSX.Element {
   
   return (
-      <div className="row">
-        <div className="api-info" >{props.api}</div>
+      <div className="row" color={props.backgroundColor}>
+        <div className="api-info">{props.api}</div>
         <div className="api-info">{props.location}</div>
         <div className="api-info">{props.status}</div>
       </div>
