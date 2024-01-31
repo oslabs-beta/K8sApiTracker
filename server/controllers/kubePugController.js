894bfdf7989438fb7860cc7f6d5b2f6a841df2de
Yiting Xiao <yitingxiao0531@gmail.com>
2024-01-31 14:27:41 -0500

    added getApiInfo controller to kubePugController

---
+++ server/controllers/kubePugController.js
@@ -0,0 +1,38 @@
+const kubePugController = {};
+kubePugController.getApiInfo = (req, res, next) => {
+  fetch('https://kubepug.xyz/data/data.json')
+    .then(apiInfo => 
+      apiInfo.json())
+    .then(data => {
+      // console.log('data', data)
+      const apiCache = {};
+      const apiInfo = []
+      const keys = ['version', 'kind', 'description', 'introduced_version', 'deprecated_version', 'removed_version','replacement'];
+      for (const apiObject of data) {
+      //  console.log('apiObject', apiObject)
+        for (const key in apiObject){
+          if(keys.includes(key)){
+            apiCache[key] = apiObject[key]
+          }
+          apiInfo.push(apiCache)
+        }
+        // Object.assign(apiInfo, apiObjectInfo);
+      }
+      console.log('apiCache', apiCache)
+      console.log('apiInfo', apiInfo)
+    })
+    .catch(err => {
+      next(err);
+    })
+}
+
+kubePugController.getApiInfo()
+
+// catControllers.deleteCat = (req, res, next) => {
+//   //get id off url
+//   const catName = req.params.catName;
+//   //find the note and delete it
+//   Cat.findOneAndDelete(catName)
+// };    
+
+module.exports = kubePugController;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const kubePugController = require('../server/controllers/kubePugController');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
-
+ 
+app.get('/apiInfo',kubePugController.getApiInfo, (req, res) => {
+    res.status(200);
+} )
 app.listen(3000, () => {
-    console.log('Hello there, server is running on port 3000');
+  console.log('Hello there, server is running on port 3000');
 });
