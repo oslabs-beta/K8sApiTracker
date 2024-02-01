614fdbfd2e3981d0c0f7fa6c97f3fe4810eab7e6
Matt Hart <mhart1992@gmail.com>
2024-02-01 13:59:53 -0500

    Still working on deprecation compare logic. Commiting to pull remote deployment

--- server/controllers/compareController.js
+++ server/controllers/compareController.js
@@ -24,22 +24,22 @@ compareController.compare = (req, res, next) => {
       if (object.apiVersion === kubePug[object.kind].version) {
         //! MATCH FOUND
         console.log('M A T C H    F O U N D')
-        // console.log('object.kind: ', object.kind);
-        // console.log('kubePug[object.kind].version: ', kubePug[object.kind].version)
-        // console.log('---------------------------');
+        console.log('object.kind: ', object.kind);
+        console.log('kubePug[object.kind].version: ', kubePug[object.kind].version)
+        console.log('---------------------------');
 
 
-        if (!Object.values([object.kind].replacement).length) {
+        // console.log('Length of replacement property: ', Object.values(kubePug[object.kind].replacement).length);
+
+        if (!Object.values(kubePug[object.kind].replacement).length) {
           console.log('DEPRECATED and NO REPLACEMENT');
           //! DEPRECATED & NO REPLACEMENT
           object.kind.deprecationStatus = 'noReplacement';
-          console.log(' object.kind.deprecationStatus', object.kind.deprecationStatus)
         }
         else {
           console.log('DEPRECATED with REPLACEMENT AVAILABLE');
           //! DEPRECATED w/ REPLACEMENT AVAILABLE
           object.newVersion = kubePug[object.kind].replacement.version;
-          console.log('object.newVersion', object.newVersion)
         }
 
         // Add kubePug data properties to clusterData
