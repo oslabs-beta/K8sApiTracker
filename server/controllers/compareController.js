f440a99df5fc1c0fd5625cd5dc6bfc9cf92c02ae
Matt Hart <mhart1992@gmail.com>
2024-02-01 14:33:44 -0500

    All data is now being sent on res.locals.clusterData. Working with frontend team to render properly now.

--- server/controllers/compareController.js
+++ server/controllers/compareController.js
@@ -16,25 +16,15 @@ compareController.compare = (req, res, next) => {
 
 
     if (kubePug.hasOwnProperty(object.kind)) {
-      // console.log('clusterData Kind: ', object.kind)
-      // console.log('clusterData Version: ', object.apiVersion)
-      // console.log('kubePug version: ', kubePug[object.kind].version);
-      // console.log('---------------------------');
 
       if (object.apiVersion === kubePug[object.kind].version) {
         //! MATCH FOUND
-        console.log('M A T C H    F O U N D')
-        console.log('object.kind: ', object.kind);
-        console.log('kubePug[object.kind].version: ', kubePug[object.kind].version)
-        console.log('---------------------------');
-
-
-        // console.log('Length of replacement property: ', Object.values(kubePug[object.kind].replacement).length);
 
         if (!Object.values(kubePug[object.kind].replacement).length) {
           console.log('DEPRECATED and NO REPLACEMENT');
           //! DEPRECATED & NO REPLACEMENT
           object.kind.deprecationStatus = 'noReplacement';
+          object.newVersion = false;
         }
         else {
           console.log('DEPRECATED with REPLACEMENT AVAILABLE');
@@ -44,7 +34,6 @@ compareController.compare = (req, res, next) => {
 
         // Add kubePug data properties to clusterData
         object.description = kubePug[object.kind].description;
-        // console.log('object.description', object.description)
         object.deprecationStatus = kubePug[object.kind].deprecationStatus;
         found = true;
       }
