2d1df77c43c9c6b6a323abd9074514aa3fe01e09
Yiting Xiao <yitingxiao0531@gmail.com>
2024-01-31 20:21:16 -0500

    added logic to selectively passing data

---
+++ server/controllers/compareController.js
@@ -0,0 +1,31 @@
+
+const compareController = {};
+
+compareController.compare = (req, res, next) => {
+  const kubePug = res.locals.apiInfo;
+  const clusterData = res.locals.clusterData;
+
+
+  // Declare resultObj
+  const resultObj = {};
+  const checked = {};
+  // Iterate through clusterData
+  for (const object of clusterData){
+    let found = false;
+    if(kubePug.hasOwnProperty(object.kind)){
+      if(object.apiVersion === kubePug[object.kind].version){
+        object.newVersion = kubePug[object.kind].replacement.version;
+        object.description = kubePug[object.kind].description;
+        found = true;
+      }
+    }
+    if (!found) {
+      object.newVersion = false;
+      object.description = false;
+    }
+  }
+//   console.log('res.locals.clusterData', clusterData)
+  return next();
+}
+// 
+module.exports = compareController;
--- server/controllers/kubePugController.js
+++ server/controllers/kubePugController.js
@@ -1,31 +1,24 @@
 const kubePugController = {};
 kubePugController.getApiInfo = (req, res, next) => {
   fetch('https://kubepug.xyz/data/data.json')
-    .then(apiInfo => 
-      apiInfo.json())
+    .then(apiInfo => apiInfo.json())
     .then(data => {
-      // console.log('data', data)
-      const apiCache = {};
-      const apiInfo = []
-      const keys = ['version', 'kind', 'description', 'introduced_version', 'deprecated_version', 'removed_version','replacement'];
+      const apiInfo = {};
+
       for (const apiObject of data) {
-      //  console.log('apiObject', apiObject)
-        for (const key in apiObject){
-          if(keys.includes(key)){
-            apiCache[key] = apiObject[key]
-          }
-          apiInfo.push(apiCache)
+        apiInfo[apiObject.kind] = {
+          version: apiObject.version,
+          replacement: apiObject.replacement,
+          description: apiObject.description
         }
       }
-      // console.log('apiCache', apiCache)
-      console.log('apiInfo', apiInfo)
+      
+      // console.log('apiInfo', apiInfo)
       res.locals.apiInfo = apiInfo;
       return next();
     })
-    .catch(err => 
-      next(err)
-    )
-}
+    .catch(err => next(err))
+};
 
 
 // catControllers.deleteCat = (req, res, next) => {
