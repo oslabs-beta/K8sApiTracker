e7d030a2dda876d31abc3685d9ffcb68f5665499
Matt Hart <mhart1992@gmail.com>
2024-02-09 10:16:48 -0500

    Fixed bug so typescript migration is now complete

--- server/controllers/kubePugController.js
+++ server/controllers/kubePugController.js
@@ -6,23 +6,23 @@ const kubePugController = {
         fetch('https://kubepug.xyz/data/data.json')
             .then(apiInfo => apiInfo.json())
             .then((data) => {
-            console.log('data', data);
-            const apiInfo = {};
-            for (const apiObject of data) {
-                apiInfo[apiObject.kind] = {
-                    version: apiObject.version,
-                    replacement: apiObject.replacement,
-                    description: apiObject.description
-                };
-            }
-            ;
-            res.locals.apiInfo = apiInfo;
-            console.log('res.locals.apiInfo', res.locals.apiInfo);
-            return next();
-        })
+                // console.log('data', data);
+                const apiInfo = {};
+                for (const apiObject of data) {
+                    apiInfo[apiObject.kind] = {
+                        version: apiObject.version,
+                        replacement: apiObject.replacement,
+                        description: apiObject.description
+                    };
+                }
+                ;
+                res.locals.apiInfo = apiInfo;
+                // console.log('res.locals.apiInfo', res.locals.apiInfo);
+                return next();
+            })
             .catch(err => {
-            return next(err);
-        });
+                return next(err);
+            });
     }
 };
 module.exports = kubePugController;
