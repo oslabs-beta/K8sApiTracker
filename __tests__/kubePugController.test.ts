8b1543f8db7228cccea9e61bc674518e2e2b8320
Yiting Xiao <yitingxiao0531@gmail.com>
2024-02-10 11:55:51 -0500

    added mocked request and mocked response to test kubepubController
    Co-authored-by: Thomas Ortiz thomasortiz444@gmail.com

--- __tests__/kubePugController.test.ts
+++ __tests__/kubePugController.test.ts
@@ -1,18 +1,35 @@
 import { NextFunction, Request, Response } from "express";
 const kubePugController = require('../server/controllers/kubePugController.js');
-
+import { getMockReq } from '@jest-mock/express';
 
 describe("kubePugController", () => {
 
-    test('two plus two is four', () => {
-        expect(2 + 2).toBe(4);
+  test("checking kugepug Controller", async()=>{
+    // create a mock request using the jest-mock/express library
+    const mockReq: Request = getMockReq();
+    // create a mock response
+    const mockRes: any =  {
+      locals: {}
+    };
+    //create a mock next object
+    const mockNext = jest.fn();
+    
+    // Wrap the middleware call in a Promise to wait for next to be called
+    await new Promise((resolve, reject) => {
+      kubePugController.getApiInfo(mockReq, mockRes, (err: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(mockRes.locals.apiInfo); // Resolve with apiInfo for potential further assertions
+        }
+      });
     });
-    // invoke our kubePugController
-    
-    // expect it to return next...
-    
-    //check the fetch
+    //After the middleware has completed, print the res.locals.apiInfo
+    console.log(mockRes.locals.apiInfo);
+    })
+  })
 
+    //check the fetch
     //expect it to store res.locals.apiInfo which will...
         // be an array
         // contain objects in it
@@ -21,5 +38,3 @@ describe("kubePugController", () => {
             // a version property, which is a string
             // a kind property, which is a string
             // a description property, which is a string
-            
-})
