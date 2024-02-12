33d028fe89723c69aa1abf2063b6bf435ee35aab
Yiting Xiao <yitingxiao0531@gmail.com>
2024-02-12 15:13:30 -0500

    added typing for dependency state in mainPageContainer
    Co-authored_by Thomad Ortiz thomasortiz444@gmail.com

---
+++ __tests__/RowHeader.test.ts
@@ -0,0 +1,12 @@
+// import { render, screen } from '@testing-library/react';
+// import App from '../src/RowHeader.tsx';
+// import MainPageContainer from '../src/MainPageContainer.tsx';
+
+  
+// describe('Testing on RowHeader', () => {
+// test('On load of web page, RowHeader should load', () => {
+//     render (< App />)
+
+
+//   });
+// })
