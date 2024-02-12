fc8f3526dc7cd3d508b4b9d2faa047469bfed211
Yiting Xiao <yitingxiao0531@gmail.com>
2024-02-12 16:47:38 -0500

    fixed server testing file
    Co-authored-by Thomas Ortiz thomasortiz444@gmail.com

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import App from '../src/App'
+  
+describe('Testing on RowHeader', () => {
+
+  test('On load of web page, RowHeader should load', () => {
+    // const app = render(<App />)
+    expect(2+2).toBe(4)
+    });
+}) 
