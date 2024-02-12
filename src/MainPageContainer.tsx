6723484095b44a67c7b86caa02ee6f061ad9a8a9
Tyler Savitsky <tsavitsky11@gmail.com>
2024-02-12 16:09:47 -0500

    disabled buttons when acquiring dependencies

--- src/MainPageContainer.tsx
+++ src/MainPageContainer.tsx
@@ -12,28 +12,58 @@ export default function MainPageContainer(): React.JSX.Element {
 
   // initialize our state
   const [dependencies, setDependencies] = useState([]);
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(null);
+  const [showRowHeader, setRowHeader] = useState(false);
   const [filters, setFilters] = useState<string[]>(['stable', 'updateAvailable', 'removed']);
   const [scanButtonText, setScanButtonText] = useState<string[]>(['Scan a directory of .yaml files', 'Scan a Helm chart before you install it']);
+  const [fetchEndpoint, setFetchEndpoint] = useState<string[]>(['/dependencies', '/helm']);
   
   // make a fetch request to our backend at the route /dependencies.
   // call a useEffect here to fetch our data when the page loads and update state.
   // Our dependency array is an empty array, so that this only happens once on page load
-  useEffect(() => {
+  // useEffect(() => {
+  //   async function getDependencies() {
+  //     let response = await fetch('/dependencies');
+  //     const responseData = await response.json()
+  //     //sort the response data alphabetically by deprecation status
+  //     responseData.sort((a: any, b:any) => {
+  //       if (a.deprecationStatus < b.deprecationStatus) return -1;
+  //       if (a.deprecationStatus > b.deprecationStatu) return 1;
+  //       return 0;
+  //     })
+  //     setDependencies(responseData);
+  //     setLoading(false);
+  //   }
+  //   getDependencies();
+  // }, []);
+
+  // create two versions of the scanButton - one for directory scans, one for helm chart scans
+  // change the endpoint of fetch request based on which button is clicked
+  // throttle the buttons until the new rows load and clear out the old rows
+  let scanButtons: React.JSX.Element[] = [];
+  const handleClick = (endpoint: string) => {
+    setDependencies([]);
     async function getDependencies() {
-      let response = await fetch('/dependencies');
-      const responseData = await response.json()
-      //sort the response data alphabetically by deprecation status
-      responseData.sort((a: any, b:any) => {
-        if (a.deprecationStatus < b.deprecationStatus) return -1;
-        if (a.deprecationStatus > b.deprecationStatu) return 1;
-        return 0;
-      })
+    setLoading(true);
+    setRowHeader(true);
+    let response = await fetch(`${endpoint}`);
+    const responseData = await response.json();
+    //sort the response data alphabetically by deprecation status
+    responseData.sort((a: any, b:any) => {
+      if (a.deprecationStatus < b.deprecationStatus) return -1;
+      if (a.deprecationStatus > b.deprecationStatu) return 1;
+      return 0;
+    })
       setDependencies(responseData);
       setLoading(false);
     }
     getDependencies();
-  }, []);
+  }
+
+  for(let i = 0; i < scanButtonText.length; i++) {
+    scanButtons.push(<ScanButton key={`scanButton${i}`} text={scanButtonText[i]} onClick={() => handleClick(fetchEndpoint[i])}/>);
+  };
+
 
   function updateFilters(status: string){
     //if the filter is already applied, remove it from the filters array
@@ -58,18 +88,13 @@ export default function MainPageContainer(): React.JSX.Element {
     }
   }
 
-  // create two versions of the scanButton - one for directory scans, one for helm chart scans
-  let scanButtons: React.JSX.Element[] = [];
-  for(let i = 0; i < scanButtonText.length; i++) {
-    scanButtons.push(<ScanButton key={`scanButton${i}`} text={scanButtonText[i]}/>);
-  };
 
   return (
     <div className='mainPageContainer'>
       <div id='scanButtonContainer'>
-        {scanButtons}
+        {isLoading? null : scanButtons };
       </div>
-      <RowHeader key={'row-header-key'} api='API' status='STATUS' location='LOCATION' stable='STABLE VERSION' notes='NOTES' filters={filters} filter={updateFilters}/>
+      {showRowHeader? <RowHeader key={'row-header-key'} api='API' status='STATUS' location='LOCATION' stable='STABLE VERSION' notes='NOTES' filters={filters} filter={updateFilters}/> : false}
       {isLoading? <SpinningCircles className="content-loading" /> : null}
       <div className='row-content-container'>
         {rows}        
--- src/ScanButton.tsx
+++ src/ScanButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 type ScanButtonProps = {
-  text: string
+  text: string,
+  onClick: React.MouseEventHandler<HTMLButtonElement>
 };
 
 export default function ScanButton(props:ScanButtonProps):React.JSX.Element {
     return (
-          <button className='scanButton'>{props.text}</button>
-      )
+          <button className='scanButton' onClick={props.onClick}>{props.text}</button>
+    )
 }
