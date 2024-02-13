d80135252b92b33141f86754c8bffb90eb69f55b
thomas444ortiz <thomasortiz444@gmail.com>
2024-02-13 12:12:21 -0500

    deleted files tbd
    Co-authored-by Yiting Xiao yitingxiao0531@gmail.com

---
+++ __tests__/FilterDropdown.test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import RowHeader from '../src/FilterDropdown'
+
+describe('Testing on FilterDropdown', () => {
+    test('Test our FilterDropdown component', () => {
+        
+
+
+    });
+})
