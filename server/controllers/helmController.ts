d41bc5cbfee6ad40a2984a3cb4e208c43619627b
Matt Hart <mhart1992@gmail.com>
2024-02-13 12:04:20 -0500

    Add comments and fix TypeScript errors in helmController

--- server/controllers/helmController.ts
+++ server/controllers/helmController.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
 type getUserInput = (req: Request, res: Response, next: NextFunction) => void
-
 type HelmController = {
     getUserInput: getUserInput;
 }
-
 type NewObj = {
     name: string,
     kind: string,
@@ -13,13 +11,16 @@ type NewObj = {
     namespace: string,
     image: string
 }
-
+type MatchedData = String[];
 type CleanData = NewObj[];
 
 const helmController: HelmController = {
 
+
     getUserInput: async (req, res, next) => {
         const childProcess = require('child_process');
+
+        /* 'sh' function will run a child process to execute a command in the user's terminal; the command being a helm install w/ dry-run and debug flags for whatever helm chart the user inputs in the front end. The returned object has a 'manifest' property that represents the .yaml files of that helm chart. We then use a regex expression to parse that data and store into an array. */
 
         async function sh(cmd_to_execute: string) {
             console.log('Inside of sh function');
@@ -44,6 +45,8 @@ const helmController: HelmController = {
             });
         }
 
+        /* Get the user input which is the helm install command copied from a chart repo, like Artifact Hub. Then, reformat the string to include the dry-run and debug flags with a json output at the end. Finally, call the 'sh' function above with the cleaned user input to execute the dry-run chart install */
+
         //! ---------------- HARD CODED VERSION - UPDATE BEFORE PRODUCTION -----------------
         // Check users input and concat to proper syntax for helm install as dry-run in debug mode
         let userInput = 'helm install my-prometheus prometheus-community/prometheus --version 25.11.1';
@@ -55,13 +58,15 @@ const helmController: HelmController = {
 
         console.log('Cleaned User Input: ', userInput);
 
-        const matchedData = await sh(userInput);
+        const matchedData: MatchedData = await sh(userInput);
+
+        /* Now that we have the raw properties back from the chart install, iterate through that array, creating a new object whenever we hit an element that starts with "Source: ". Populate that object with the next two elements which should be the apiVersion and kind. Then hard code the namespace and image properties which will be default for the dry-run chart installs. This object is in the same format as the object that we persist through our middleware when scanning a users cluster, allowing us to render consistent data on the front end regardless of 'scan' type. */
 
         const cleanMatchedData: CleanData = [];
 
         for (let i = 0; i < matchedData.length; i++) {
-            // Check for name (aka: 'Source: ')
 
+            // Check for name (aka: 'Source: ')
             if (matchedData[i].slice(0, 3) === 'Sou') {
 
                 const newObj: NewObj = {}
