d239166d9a3490fe9379b8031e9115b156cfd68f
Tyler Savitsky <tsavitsky11@gmail.com>
2024-02-13 20:10:50 -0500

    Added the input field to the Helm scan section, with an changeHandler that sets HelmChartPath state, then modified the scanButton click handler to send this state with a POST request to our server

--- src/MainPageContainer.tsx
+++ src/MainPageContainer.tsx
@@ -30,35 +30,66 @@ export default function MainPageContainer(): React.JSX.Element {
   const [isLoading, setLoading] = useState<boolean>(false);
   const [showRowHeader, setRowHeader] = useState(false);
   const [filters, setFilters] = useState<string[]>(['stable', 'updateAvailable', 'removed']);
-  const [scanButtonText, setScanButtonText] = useState<string[]>(['Scan a directory of .yaml files', 'Scan a Helm chart before you install it']);
+  const [scanButtonText, setScanButtonText] = useState<string[]>(['Scan a directory of .yaml files', 'Scan Helm chart']);
   const [fetchEndpoint, setFetchEndpoint] = useState<string[]>(['/dependencies', '/helm']);
+  const [helmChartPath, setHelmChartPath] = useState<string>('');
 
 
   // create two versions of the scanButton - one for directory scans, one for helm chart scans
-  // change the endpoint of fetch request based on which button is clicked
+  // change the fetch request based on which button is clicked, and if it is a Helm scan attach the input data
   // disable the buttons until the new rows load and clear out the old rows
   let scanButtons: React.JSX.Element[] = [];
   const handleClick = (endpoint: string) => {
     setDependencies([]);
     async function getDependencies(): Promise<void> {
-      setLoading(true);
-      let response = await fetch(`${endpoint}`);
-      const responseData = await response.json();
+      if (endpoint === '/helm') {
+        setLoading(true);
+        let response = await fetch(`/helm`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            helmChartPath: helmChartPath
+          })
+        });
+        const responseData = await response.json();
+        //sort the response data alphabetically by deprecation status
+        responseData.sort((a: any, b:any) => {
+          if (a.deprecationStatus < b.deprecationStatus) return -1;
+          if (a.deprecationStatus > b.deprecationStatu) return 1;
+          return 0;
+        })
+        setRowHeader(true);
+        setDependencies(responseData);
+        setHelmChartPath('');
+        setLoading(false);
+      } else if (endpoint === '/dependencies') {
+        setLoading(true);
+        let response = await fetch(`${endpoint}`);
+        const responseData = await response.json();
       //sort the response data alphabetically by deprecation status
-      responseData.sort((a: any, b:any) => {
-        if (a.deprecationStatus < b.deprecationStatus) return -1;
-        if (a.deprecationStatus > b.deprecationStatu) return 1;
-        return 0;
-      })
-      setRowHeader(true);
-      setDependencies(responseData);
-      setLoading(false);
+        responseData.sort((a: any, b:any) => {
+          if (a.deprecationStatus < b.deprecationStatus) return -1;
+          if (a.deprecationStatus > b.deprecationStatu) return 1;
+          return 0;
+        })
+        setRowHeader(true);
+        setDependencies(responseData);
+        setLoading(false);
+      }
     }
     getDependencies();
   }
 
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHelmChartPath(event.target.value);
+  }
+
   for(let i = 0; i < scanButtonText.length; i++) {
-    scanButtons.push(<ScanButton key={`scanButton${i}`} text={scanButtonText[i]} onClick={() => handleClick(fetchEndpoint[i])} isLoading={isLoading}/>);
+    scanButtons.push(<ScanButton id={i} key={`scanButton${i}`} text={scanButtonText[i]} onClick={() => handleClick(fetchEndpoint[i])} isLoading={isLoading}/>);
   };
 
 
@@ -89,7 +120,12 @@ export default function MainPageContainer(): React.JSX.Element {
   return (
     <div id='mainPageContainer'>
       <div id='scanButtonContainer'>
-        {scanButtons}
+        {scanButtons[0]}
+        <div id='helmSection'>
+          <p>Or scan a Helm Chart before installing:</p>
+          <input type='text' id='helmChartInput' name='helmChartInput' placeholder='Copy absolute path' onChange={handleChange}/>
+          {scanButtons[1]}
+        </div>
       </div>
       {/* <DashboardContainer /> */}
       {showRowHeader? <RowHeader key={'row-header-key'} api='API' status='STATUS' location='LOCATION' stable='STABLE VERSION' notes='NOTES' filters={filters} filter={updateFilters}/> : false}
