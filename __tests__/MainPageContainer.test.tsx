80673640bdf2e53bf343c0b4af28079fe2a1c131
Yiting Xiao <yitingxiao0531@gmail.com>
2024-02-13 23:13:37 -0500

    finished with Row testing, please review

--- __tests__/MainPageContainer.test.tsx
+++ __tests__/MainPageContainer.test.tsx
@@ -1,31 +1,31 @@
 /**
- * @jest-environment jsdom
- */
+* @jest-environment jsdom
+*/
 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import MainPageContainer from '../src/MainPageContainer'
-// import MainPageContainer from '../src/';  
-describe('Testing on MainPageContainer', () => {
-    test('Test our MainPageContainer component', () => {
-        //render MainPageContainer
-        const {container} = render(<MainPageContainer/>);
-      // expect the component to exist
-      const scanButtonContainer = screen.queryByTestId('scanButtonContainer');
-      expect(scanButtonContainer).toBeDefined();
-      //test on conditional rendering of RowHeader component
-    //   if(!showRowHeader){
-    //     expect(<RowHeader/>).toBeNull();
-    //   } else {
-    //     expect(<RowHeader/>).toBeDefined();
-    //   };
-    //   //test on conditional rendering of spinningCircles component
-    //   if (isLoading){
-    //     expect(<SpinningCircles/>).toBeDefined();
-    //   };
-      const rowContentContainer = container.getElementsByClassName('row-content-container');
-      expect(rowContentContainer).toBeDefined();
+import MainPageContainer from '../src/MainPageContainer';
 
-     
-    });
+// import MainPageContainer from '../src/'; 
+describe('Testing on MainPageContainer', () => {
+test('Test our MainPageContainer component', () => {
+//render MainPageContainer
+const {container} = render(<MainPageContainer/>);
+// expect the component to exist
+const scanButtonContainer = screen.queryByTestId('scanButtonContainer');
+expect(scanButtonContainer).toBeDefined();
+const rowContentContainer = container.getElementsByClassName('row-content-container');
+expect(rowContentContainer).toBeDefined();
+});
+// test('render rowHeader if showRowHeader is true', () => {
+// const {container} = render(<MainPageContainer/>);
+// const roe
+// // You can add more specific queries based on your actual component structure
+// expect(screen.getByText('API')).toBeInTheDocument();
+// expect(screen.getByText('STATUS')).toBeInTheDocument();
+// })
 })
+
+
+
+
