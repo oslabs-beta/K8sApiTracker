b8ac266dc948f2f7339cbf6f87b22ceb6a929be8
Yiting Xiao <yitingxiao0531@gmail.com>
2024-02-14 20:13:14 -0500

    added button check for ScanButtonsContainer test

--- __tests__/ScanButtonsContainer.test.tsx
+++ __tests__/ScanButtonsContainer.test.tsx
@@ -2,16 +2,16 @@
  * @jest-environment jsdom
  */
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import ScanButtonContainer from '../src/ScanButtonsContainer'
 
 describe('Test on ScanButtonContainer', () => {
-  test('All related components should render', () => {
+  test('Button should render after page is loaded', () => {
     const handleClick = jest.fn();
     const repoHandleChange = jest.fn();
     const chartHandleChange =jest.fn();
-    render(<ScanButtonContainer key={`scanButton`} handleClick={() => handleClick('/dependencies')} isLoading={true} repoHandleChange={repoHandleChange()} chartHandleChange={chartHandleChange()}/>);
+    render(<ScanButtonContainer handleClick={handleClick} isLoading={false} repoHandleChange={repoHandleChange()} chartHandleChange={chartHandleChange()}/>);
     const localButton = screen.getByText('Scan Local Cluster');
     expect(localButton).toBeDefined();
     const helmButton = screen.getByText('Scan Helm chart');
@@ -20,6 +20,33 @@ describe('Test on ScanButtonContainer', () => {
     expect(repoInput).toBeDefined();
     const installInput = screen.getByPlaceholderText('Install cmd');
     expect(installInput).toBeDefined();
+    fireEvent.click(localButton);
+    fireEvent.click(localButton);
+    expect(handleClick).toHaveBeenCalledTimes(2);
+    fireEvent.click(helmButton);
+    expect(handleClick).toHaveBeenCalledTimes(3);
+    fireEvent.click(localButton);
+    expect(handleClick).toHaveBeenCalledTimes(4);
 });
-
+test('Button should not render when page is loading', () => {
+    const handleClick = jest.fn();
+    const repoHandleChange = jest.fn();
+    const chartHandleChange =jest.fn();
+    render(<ScanButtonContainer handleClick={handleClick} isLoading={true} repoHandleChange={repoHandleChange()} chartHandleChange={chartHandleChange()}/>);
+    const localButton = screen.getByText('Scan Local Cluster');
+    expect(localButton).toBeDefined();
+    const helmButton = screen.getByText('Scan Helm chart');
+    expect(helmButton).toBeDefined();
+    const repoInput = screen.getByPlaceholderText('Repo cmd');
+    expect(repoInput).toBeDefined();
+    const installInput = screen.getByPlaceholderText('Install cmd');
+    expect(installInput).toBeDefined();
+    fireEvent.click(localButton);
+    fireEvent.click(localButton);
+    expect(handleClick).toHaveBeenCalledTimes(0);
+    fireEvent.click(helmButton);
+    expect(handleClick).toHaveBeenCalledTimes(0);
+    fireEvent.click(helmButton);
+    expect(handleClick).toHaveBeenCalledTimes(0);
+});
 });
