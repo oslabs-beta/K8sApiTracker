d13d91ebb094de54973c818f860803434114cd2b
Yiting Xiao <yitingxiao0531@gmail.com>
2024-04-14 11:08:27 -0400

    change all require statement to import statement in dependencyScraper middleware

--- server/controllers/compareController.ts
+++ server/controllers/compareController.ts
@@ -43,5 +43,4 @@ const compareController = {
 
 };
 
-// 
 export default compareController;
--- server/controllers/dependencyScraper.ts
+++ server/controllers/dependencyScraper.ts
@@ -1,6 +1,6 @@
-const { glob } = require('glob');
-const fs = require('fs');
-const path = require('path');
+import glob from 'glob';
+import fs from 'fs';
+import path from 'path';
 import { Request, Response, NextFunction } from 'express';
 import { DependencyScraperController, ApiObj, Options } from '../types'
 
@@ -12,54 +12,54 @@ const dependencyScraperController: DependencyScraperController = {
         const getYaml = async () => {
             try {
                 const options: Options = {
-                  cwd: path.resolve(__dirname, '../../../'),
-                  absolute: true,
-                  ignore: 'node_modules/**'
+                    cwd: path.resolve(__dirname, '../../../'),
+                    absolute: true,
+                    ignore: 'node_modules/**'
                 };
-                const response: string[] = await glob(['**/*.yaml','**/*.yml'], options);
+                const response: string[] = await glob(['**/*.yaml', '**/*.yml'], options);
                 return response;
-              }
-              catch(e) {
-                  console.log('error scraping directory for yaml filepaths');
-              }
+            }
+            catch (e) {
+                console.log('error scraping directory for yaml filepaths');
+            }
         };
 
         getYaml()
-        .then((data) => {
-            try {
-                //create our array to hold all of our api objects
-                const dependencies: ApiObj[] = [];
-                // iterate through the files
-                for(const file of data) {
-                    const obj: ApiObj = {};
-                    // use fs method, to get the content from the yaml file
-                    const content: string = fs.readFileSync(file, 'utf-8');   
-                    //add all properties to an object using regex to scrape yaml file for values
-                    const properties = ['apiVersion', 'kind', 'name', 'namespace', 'image'];
-                    const defaults = ['NA', 'NA', 'NA', 'Default', 'NA'];
-                    for (let i = 0; i < properties.length; i++){
-                        try {
-                            const version = new RegExp(`${properties[i]}:\\s*(.*)`);
-                            const array = version.exec(content);
-                            obj[properties[i]] = array[0].replace(`${properties[i]}:`, '').trim();                        
+            .then((data) => {
+                try {
+                    //create our array to hold all of our api objects
+                    const dependencies: ApiObj[] = [];
+                    // iterate through the files
+                    for (const file of data) {
+                        const obj: ApiObj = {};
+                        // use fs method, to get the content from the yaml file
+                        const content: string = fs.readFileSync(file, 'utf-8');
+                        //add all properties to an object using regex to scrape yaml file for values
+                        const properties = ['apiVersion', 'kind', 'name', 'namespace', 'image'];
+                        const defaults = ['NA', 'NA', 'NA', 'Default', 'NA'];
+                        for (let i = 0; i < properties.length; i++) {
+                            try {
+                                const version = new RegExp(`${properties[i]}:\\s*(.*)`);
+                                const array = version.exec(content);
+                                obj[properties[i]] = array[0].replace(`${properties[i]}:`, '').trim();
+                            }
+                            catch {
+                                obj[properties[i]] = defaults[i]; // if the pattern is not available, use the default
+                            }
                         }
-                        catch {
-                            obj[properties[i]] = defaults[i]; // if the pattern is not available, use the default
-                        }
+                        // also add in the filepath to send to the frontend
+                        obj.location = file;
+                        dependencies.push(obj);
                     }
-                    // also add in the filepath to send to the frontend
-                    obj.location = file;
-                    dependencies.push(obj);
+                    //save the data on locals and go to next middleware function
+                    res.locals.clusterData = dependencies;
+                    return next();
                 }
-                //save the data on locals and go to next middleware function
-                res.locals.clusterData = dependencies;
-                return next();                   
-            }
-            catch (err){
-                return next(err || 'There was an error sraping dependencies');
-            }   
-        })   
-    }    
+                catch (err) {
+                    return next(err || 'There was an error sraping dependencies');
+                }
+            })
+    }
 };
 
-module.exports = dependencyScraperController;
+export default dependencyScraperController;
